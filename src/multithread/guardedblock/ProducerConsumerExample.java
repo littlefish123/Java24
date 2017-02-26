d0832849c4437e329930d0b39507ba1900ccdbe9
littlefish123 <n.selina@ymail.com>
2017-02-26 11:40:00 +0800

    first commit

---
+++ src/multithread/guardedblock/ProducerConsumerExample.java
@@ -0,0 +1,15 @@
+/* http://docs.oracle.com/javase/tutorial/essential/concurrency/memconsist.html */
+
+package multithread.guardedblock;
+
+
+public class ProducerConsumerExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   Drop drop = new Drop();
+	        (new Thread(new Producer(drop))).start();
+	        (new Thread(new Consumer(drop))).start();
+	}
+
+}
